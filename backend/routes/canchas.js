e30ec4c37c74ad487128adef0dc665ea9283a9e6
Angel14Salva <salvadorluis290@gmail.com>
2026-06-18 22:11:39 +0000

    fix: corregir rutas canchas con canchaController OOP

--- backend/routes/canchas.js
+++ backend/routes/canchas.js
@@ -3,11 +3,9 @@ const router  = express.Router();
 const canchaController = require('../controllers/canchaController');
 const verifyToken = require('../middleware/verifyToken');
 const checkRole   = require('../middleware/checkRole');
-
-router.get('/',                    getAll);
-router.get('/:id',                 getById);
-router.get('/:id/disponibilidad',  getDisponibilidad);
+router.get('/',                    canchaController.getAll.bind(canchaController));
+router.get('/:id/disponibilidad',  canchaController.getDisponibilidad.bind(canchaController));
+router.get('/:id',                 canchaController.getById.bind(canchaController));
 router.post('/',   verifyToken, checkRole('admin'), canchaController.create.bind(canchaController));
 router.put('/:id', verifyToken, checkRole('admin'), canchaController.update.bind(canchaController));
-
 module.exports = router;
